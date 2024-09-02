d9d9cd33c6b980a5e5e75db78d618cec21e889fb
liswa's sunshine <socksdirty23@gmail.com>
2024-09-02 11:07:48 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put sesson id",
+SESSION_ID: process.env.SESSION_ID || "NrVTRDyR#1P56gsWhA_1C8as5sLqxGt3fVSz2JD4bDoN8gF-RbKk",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/357b75726739a8b9c68de.jpg",
-SUDO_NB: process.env.SUDO_NB || "ඔයාගෙ owner number එක දෙන්න",
+SUDO_NB: process.env.SUDO_NB || "263718036108",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: "true"
